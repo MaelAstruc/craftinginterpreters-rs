ff978b7f3b89eba2be7a4639063635cc96456f86
Mael Astruc--Le Souder <mael.astruc@gmail.com>
2023-06-01 22:36:44 +0200

    add main() to Lox struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+struct Lox {
+}
+
+impl Lox {
+    fn main(&self, args: &mut [&str]) {
+        if args.len() > 1 {
+            println!("Usage: stata [script]")
+        } else if args.len() == 1 {
+            self.run_file(args[0])
+        } else {
+            self.run_prompt()
+        }
+    }
+
+    pub fn run_file(&self, filepath: &str) {
+        println!("Run file path: {}", filepath)
+    }
+
+    pub fn run_prompt(&self) {
+        println!("Run prompt")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input: [&str; 1] = ["C:/Users/Mael/Documents/Temp/test.lox"];
+    let lox: Lox = Lox {};
+    lox.main(&mut input)
 }
