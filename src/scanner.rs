350b4731320b5fd896438c663a387875bbb7b4c0
Mael Astruc--Le Souder <mael.astruc@gmail.com>
2023-06-04 20:41:13 +0200

    complete scan_token()

--- src/scanner.rs
+++ src/scanner.rs
@@ -27,19 +27,42 @@ impl Scanner {
     fn scan_token(&mut self) {
         let c: char = self.advance();
         match c {
-          '(' => self.add_token(TokenType::LeftParen, "".to_string()),
-          ')' => self.add_token(TokenType::RightParen, "".to_string()),
-          '{' => self.add_token(TokenType::LeftBrace, "".to_string()),
-          '}' => self.add_token(TokenType::RightBrace, "".to_string()),
-          ',' => self.add_token(TokenType::Comma, "".to_string()),
-          '.' => self.add_token(TokenType::Dot, "".to_string()),
-          '-' => self.add_token(TokenType::Minus, "".to_string()),
-          '+' => self.add_token(TokenType::Plus, "".to_string()),
-          ';' => self.add_token(TokenType::SemiColon, "".to_string()),
-          '*' => self.add_token(TokenType::Star, "".to_string()),
-          _ => ()
+            '(' => self.add_token(TokenType::LeftParen, "".to_string()),
+            ')' => self.add_token(TokenType::RightParen, "".to_string()),
+            '{' => self.add_token(TokenType::LeftBrace, "".to_string()),
+            '}' => self.add_token(TokenType::RightBrace, "".to_string()),
+            ',' => self.add_token(TokenType::Comma, "".to_string()),
+            '.' => self.add_token(TokenType::Dot, "".to_string()),
+            '-' => self.add_token(TokenType::Minus, "".to_string()),
+            '+' => self.add_token(TokenType::Plus, "".to_string()),
+            ';' => self.add_token(TokenType::SemiColon, "".to_string()),
+            '*' => self.add_token(TokenType::Star, "".to_string()),
+            '!' =>
+                if self.match_char('=') {self.add_token(TokenType::BangEqual, "".to_string()) }
+                else {self.add_token(TokenType::Bang, "".to_string())},
+            '=' =>
+                if self.match_char('=') { self.add_token(TokenType::EqualEqual, "".to_string()) }
+                else {self.add_token(TokenType::Equal, "".to_string())},
+            '<' =>
+                if self.match_char('=') { self.add_token(TokenType::LessEqual, "".to_string()) }
+                else {self.add_token(TokenType::Less, "".to_string())},
+            '>' =>
+                if self.match_char('=') { self.add_token(TokenType::GreaterEqual, "".to_string()) }
+                else {self.add_token(TokenType::Greater, "".to_string())},
+            _ => ()
         }
-   }
+    }
+
+    fn match_char(&mut self, expected: char) ->bool {
+        if self.is_at_end() {
+            return false
+        }
+        if self.source.chars().nth(self.current).unwrap() != expected {
+            return false
+        }
+        self.current += 1;
+        return true
+    }
 
     fn is_at_end(&self) -> bool {
         return self.current >= self.source.len();
