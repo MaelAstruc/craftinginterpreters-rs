a17f372469898663d4b5ee6f7a691e53be18e5a2
Mael Astruc--Le Souder <mael.astruc@gmail.com>
2023-06-04 20:02:26 +0200

    add scan_tokens() to Scanner struct

--- src/scanner.rs
+++ src/scanner.rs
@@ -15,6 +15,18 @@ impl Scanner {
         Scanner {source: source, tokens: tokens, start: 0, current: 0, line: 1}
     }
 
+    pub fn scan_tokens(&mut self) {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.scan_token();
+        }
+        let last_token = Token::new(TokenType::Eof, "".to_string(), "".to_string(), self.line);
+        self.tokens.push(last_token);
+    }
+
+    fn scan_token(&mut self) {
+   }
+
     fn is_at_end(&self) -> bool {
         return self.current >= self.source.len();
     }
