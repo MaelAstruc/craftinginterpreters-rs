a02b0a7d6e6c4dcef74edf88dcf618f9cc6296bc
Mael Astruc--Le Souder <mael.astruc@gmail.com>
2023-06-28 02:06:34 +0200

    improve fmt() for Token enum

--- src/token.rs
+++ src/token.rs
@@ -12,9 +12,9 @@ impl Token { }
 impl std::fmt::Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match &self.token_type {
-            TokenType::Identifier(x) => write!(f, "{} {} {}", &self.token_type, &self.lexeme, x),
-            TokenType::String(x) => write!(f, "{} {} {}", &self.token_type, &self.lexeme, x),
-            TokenType::Number(x) => write!(f, "{} {} {}", &self.token_type, &self.lexeme, x),
+            TokenType::Identifier(x) => write!(f, "Idenfier({}) {} {}", &self.token_type, &self.lexeme, x),
+            TokenType::String(x) => write!(f, "String({}) {} {}", &self.token_type, &self.lexeme, x),
+            TokenType::Number(x) => write!(f, "Number({}) {} {}", &self.token_type, &self.lexeme, x),
             _ => write!(f, "{} {} {}", &self.token_type, &self.lexeme, &self.token_type)
         }
       }
